626ad1df3aed7ca6b97b71382db3a3f67a52489c
TanmaySalkade <tanmays1308@gmail.com>
2025-04-27 20:00:05 +0530

    Fixed route to /cources and removed unnecessary home.html

--- routes.py
+++ routes.py
@@ -1,12 +1,18 @@
-from flask import render_template, redirect, url_for
-from app import app, db
-from models import Course
-
-@app.route('/')
-def home():
-    return redirect(url_for('courses'))
+from flask import render_template
+from app import app
 
 @app.route('/courses')
 def courses():
-    all_courses = Course.query.all()
-    return render_template('courses.html', courses=all_courses)
+    courses = [
+        {
+            "title": "Python for Beginners",
+            "description": "A complete Python programming playlist.",
+            "url": "https://www.youtube.com/playlist?list=PL1A2CSdiySGJQKniwB3iMdX5T1A8zLabG"
+        },
+        {
+            "title": "SQL Tutorial",
+            "description": "Master SQL from basics to advanced.",
+            "url": "https://www.youtube.com/playlist?list=PL1A2CSdiySGK3pJzJz3pL2b1Wz0sbGzQn"
+        }
+    ]
+    return render_template('courses.html', courses=courses)
