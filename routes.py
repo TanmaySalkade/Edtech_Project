ccb449cf2ec0be2145a969cf267ab8458922236b
TanmaySalkade <tanmays1308@gmail.com>
2025-04-27 19:36:12 +0530

    Removed before_first_request to fix Render deployment

--- routes.py
+++ routes.py
@@ -10,20 +10,3 @@ def home():
 def courses():
     all_courses = Course.query.all()
     return render_template('courses.html', courses=all_courses)
-
-@app.before_first_request
-def create_courses():
-    if Course.query.count() == 0:
-        python_course = Course(
-            title="Python for Beginners",
-            description="A complete Python programming playlist.",
-            url="https://www.youtube.com/playlist?list=PL1A2CSdiySGJQKniwB3iMdX5T1A8zLabG"
-        )
-        sql_course = Course(
-            title="SQL Tutorial",
-            description="Master SQL from basics to advanced.",
-            url="https://www.youtube.com/playlist?list=PL1A2CSdiySGK3pJzJz3pL2b1Wz0sbGzQn"
-        )
-        db.session.add(python_course)
-        db.session.add(sql_course)
-        db.session.commit()
